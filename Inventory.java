34d48580e0883bc8e047aa303d68b38fe6133c72
jonathangunnells <jonathan.gunnells@yahoo.com>
2016-05-24 09:27:46 -0400

    Improved interface

---
+++ Inventory.java
@@ -0,0 +1,13 @@
+
+
+
+public class Inventory {
+    String text;
+    int quantity;
+
+
+    public Inventory(String text, int quantity) {
+        this.text = text;
+        this.quantity = quantity;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,85 @@
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void addItem(Scanner scanner, ArrayList<Inventory> items) {
+        System.out.println("Enter item to add to inventory");
+        String text = scanner.nextLine();
+        Inventory item = new Inventory(text, 0);
+        items.add(item);
+
+    }
+
+    public static void removeItem(Scanner scanner, ArrayList<Inventory> items) {
+        System.out.println("Enter item you wish to remove");
+        String numStr = scanner.nextLine();
+        int num = Integer.valueOf(numStr);
+        items.remove(num - 1);
+    }
+
+    public static void listItem(ArrayList<Inventory> items) {
+        int i = 1;
+        for (Inventory inventory : items)
+        {
+            System.out.println("[" + inventory.quantity + "] " + i + ". " + inventory.text);
+            i++;
+        }
+    }
+
+    public static void updateQuantity(Scanner scanner, ArrayList<Inventory> items) {
+        System.out.println("Enter item you with to update");
+        String itemStr = scanner.nextLine();
+        int item = Integer.valueOf(itemStr);
+
+        System.out.println("Enter new quantity");
+        String quantityStr = scanner.nextLine();
+        int quantity = Integer.valueOf(quantityStr);
+
+        Inventory inv = items.get(item - 1);
+        inv.quantity = quantity;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Inventory> items = new ArrayList();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Add Item to Inventory");
+            System.out.println("2. Remove Item");
+            System.out.println("3. List Items");
+            System.out.println("4. Update Item Quantity");
+
+            String option = scanner.nextLine();
+
+            switch (option) {
+                case "1":
+                    addItem(scanner, items);
+
+                    break;
+
+                case "2":
+                    removeItem(scanner, items);
+
+                    break;
+
+                case "3":
+                    listItem(items);
+
+                    break;
+
+                case "4":
+                    updateQuantity(scanner, items);
+
+                default:
+                    System.out.println("Invalid option.");
+
+
+            }
+
+        }
+
+    }
+}
